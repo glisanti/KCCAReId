24e8fba04a2260cf18cb47547b55dfd37f9dca1a
Iacopo Masi <iacopo.masi@gmail.com>
2014-09-17 12:52:34 +0200

    Modfied getFiles to work with all platform

--- getFiles.m
+++ getFiles.m
@@ -1,16 +1,10 @@
-if isunix
-   if ~exist('data/VIPeR_split.mat','file')
-      [status,result] = system('wget -c http://www.micc.unifi.it/lisanti/downloads/kccareid_data.zip ','-echo');
-      system('unzip kccareid_data.zip');
-      system('rm kccareid_data.zip');
-      disp('>Splits and Descriptors ready.')
-   end
-   if ~exist('kcca_package','dir')
-      system('wget -c http://www.davidroihardoon.com/Professional/Code_files/kcca_package.tar.gz');
-      system('tar -xvf kcca_package.tar.gz');
-      system('rm kcca_package.tar.gz');
-      disp('>KCCA package ready.')
-   end
-else
-   disp('Sorry it seems that you are not on UNIX so you need to download files manually.')
+if ~exist('data/VIPeR_split.mat','file')
+   disp('>Downloading and Unzipping Splits and Descriptors');
+   unzip('http://www.micc.unifi.it/lisanti/downloads/kccareid_data.zip');
+   disp('>Splits and Descriptors ready.')
+end
+if ~exist('kcca_package','dir')
+   disp('>Downloading and Unzipping KCCA package');
+   untar('http://www.davidroihardoon.com/Professional/Code_files/kcca_package.tar.gz');
+   disp('>KCCA package ready.')
 end
