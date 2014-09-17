ed5963bd5a6a6afaf79a0c9882c7c787fabe4972
Giuseppe Lisanti <glisanti@gmail.com>
2014-09-17 12:47:26 +0200

    Update kernel_expchi2.m
    Corrected Header

--- kernel_expchi2.m
+++ kernel_expchi2.m
@@ -1,6 +1,3 @@
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% (c) Copyright 2012-14  Anonymous Authors ICDSC14 #23
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % KERNEL_EXPCHI2: Compute an Exponential Kernel using Chi-Square
 % distances.
 %
@@ -11,6 +8,9 @@
 %         distances among the training examples)
 % Output: D is the Kernel matrix; md is the mean of the distances among the
 %         training examples
+% 
+% written by Lamberto Ballan (lamberto.ballan@unifi.it)
+% University of Florence, 11/05/2013
 
 function [D,md] = kernel_expchi2(X,Y,omega)
     
